322be918175a5b87d8eb193664d70ad124cc0f78
Sabine <sabine@21.lv>
2020-08-26 21:04:58 +0200

    added ternary operators for display

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
+    // React says we have to define render!!
     render() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <div> Error: {this.state.errorMessage}
-            </div>;
+            return <div>Error: {this.state.errorMessage}</div>;
         }
+
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
-        if (!this.state.errorMessage && !this.state.lat) {
-            return <div> Loading </div>;
-        }
+
+        return <div>Loading!</div>;
     }
 }
 
